68f6317d39601541998ca178d8dbc369472dc423
Rayhaan Jaufeerally <rayhaan+github@rayhaan.ch>
2018-03-07 14:36:55 +0100

    Break the tests.

--- reader_test.go
+++ reader_test.go
@@ -26,6 +26,10 @@ func cborDecoderHarnessExpectErr(t *testing.T, in []byte, errExpect error) {
 	}
 }
 
+func TestFail(t *testing.T) {
+	t.Error("Testing CI to see what happens...")
+}
+
 func TestReadInt(t *testing.T) {
 	testPatterns := []struct {
 		cbor  []byte
